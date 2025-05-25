4a004de8b5a52b2e4c351b1ef21b7dc1f42ddfd3
Marco Mambelli <marcom@fnal.gov>
2025-05-25 14:19:23 -0500

    First version of the Python project

---
+++ setup.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2025 Marco Mambelli
+# SPDX-License-Identifier: Apache-2.0
+
+from setuptools import setup
+
+setup(
+    name="recipe-unitconverter",
+    version="0.0.1",
+    packages=["src/unitconverter"],
+    install_requires=[
+        "numpy",
+    ],
+)
